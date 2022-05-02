d67fd05cd3fbfae553a3ccaf0dbbfad339ac7dd1
Гордей <gm-liker@yandex.ru>
2022-05-02 11:49:12 +0300

    task 1004 Конструкторы, создание объектов

---
+++ src/main/java/task/task10/task1004/Cat.java
@@ -0,0 +1,26 @@
+package task.task10.task1004;
+
+/*
+Построим новый бизнес-комплекс JavaRush Business Center
+*/
+
+public class Cat {
+    public static final int UNKNOWN = -1;
+    private int age;
+    private String name;
+
+    public Cat() {
+        this.age = UNKNOWN;
+        this.name = "Безыменный";
+    }
+
+    public Cat(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Cat cat2 = new Cat(50, "Васька");
+    }
+}
