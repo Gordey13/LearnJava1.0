c1b2703539c849d37932b57f6ff375fd599ba565
Гордей <gm-liker@yandex.ru>
2022-05-28 14:29:02 +0300

    Заполнение элементами ArrayList<>() {{ add; add; add; }}

--- src/main/java/task/task13/task1319/Solution.java
+++ src/main/java/task/task13/task1319/Solution.java
@@ -1,5 +1,20 @@
 package task.task13.task1319;
 
+import java.util.ArrayList;
+
+/*
+Двойные фигурные скобки
+*/
+
 public class Solution {
 
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<String>(){{
+            add("Так");
+            add("тоже");
+            add("можно");
+            add("делать");
+            add("!");
+        }};
+    }
 }
