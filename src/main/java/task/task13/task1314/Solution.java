f4d89770973b5fb4f8e0db8c79dcf631137a51a6
Гордей <gm-liker@yandex.ru>
2022-05-28 12:06:56 +0300

    Работа с ArrayList - печать, удаление, получение элемента

--- src/main/java/task/task13/task1314/Solution.java
+++ src/main/java/task/task13/task1314/Solution.java
@@ -1,5 +1,6 @@
 package task.task13.task1314;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -11,6 +12,7 @@ public class Solution {
     public static ArrayList<String> programmingLanguages = new ArrayList<>(Arrays.asList("C", "C++", "Python", "JavaScript", "Ruby", "Java", "Pascal"));
 
     public static void main(String[] args) {
+
         for (int i = 0; i < programmingLanguages.size(); i++) {
             if(programmingLanguages.get(i).equals("Pascal")){
                 programmingLanguages.remove(i);
