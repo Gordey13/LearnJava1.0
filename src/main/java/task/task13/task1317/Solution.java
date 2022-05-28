dde84b9e314911f8b25e79cc016b51969c869d52
Гордей <gm-liker@yandex.ru>
2022-05-28 12:55:22 +0300

    set get классы

---
+++ src/main/java/task/task13/task1317/Solution.java
@@ -0,0 +1,24 @@
+package task.task13.task1317;
+
+/*
+Назад в прошлое
+*/
+
+public class Solution {
+    private int[] intArray = new int[4];
+
+    public int[] getIntArray() {
+        return intArray;
+    }
+
+    public void setIntArray(int[] intArray) {
+        this.intArray = intArray;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String string = new String("Hello");
+        StringBuilder stringBuilder = new StringBuilder();
+    }
+}
+
