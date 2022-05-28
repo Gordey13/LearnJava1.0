6bc700422edaebdd2c9fb93f98be314ed9304e0c
Гордей <gm-liker@yandex.ru>
2022-05-28 14:08:43 +0300

    Создание коллекций элементов. Stack Map ArrayList HashMap

--- src/main/java/task/task13/task1318/Solution.java
+++ src/main/java/task/task13/task1318/Solution.java
@@ -1,5 +1,15 @@
 package task.task13.task1318;
 
-public class Solution {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
+public class Solution {
+    ArrayList<String> stringArrayList = new ArrayList<>();
+    Stack<String> stack = new Stack<>();
+    Map<String, ArrayList<String>> map = new HashMap<>();
+    ArrayList<Exception> exceptionArrayList = new ArrayList<>();
+    Stack fileStack = new Stack<File>();
 }
