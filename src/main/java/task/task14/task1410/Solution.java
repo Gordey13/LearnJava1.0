e25b1be9b8a0e6535ce8fc7ac148c7f5882d21a0
Гордей <gm-liker@yandex.ru>
2022-05-29 12:46:42 +0300

    Методы класса Collection HashMap.put(key, value)

--- src/main/java/task/task14/task1410/Solution.java
+++ src/main/java/task/task14/task1410/Solution.java
@@ -1,4 +1,25 @@
 package task.task14.task1410;
 
+import java.util.HashMap;
+
+/*
+Успеваемость студентов
+*/
+
 public class Solution {
+    public static HashMap<String, Double> grades = new HashMap<>();
+
+    public static void main(String[] args) {
+        addStudent("Andrey", 4.0);
+        addStudent("Andrey", 5.0);
+        addStudent("Andrey", 3.8);
+        addStudent("Andrey", 4.3);
+        addStudent("Andrey", 4.7);
+        System.out.println(grades);
+    }
+
+    public static void addStudent(String fio, Double numbers) {
+        grades.put(fio, numbers);
+    }
 }
+
