e903a8562c44022574eeab1b31fe7c959f4e1b13
Гордей <gm-liker@yandex.ru>
2022-05-29 19:02:28 +0300

    Методы класса Collection HashMap: keySet, values

--- src/main/java/task/task14/task1411/Solution.java
+++ src/main/java/task/task14/task1411/Solution.java
@@ -1,4 +1,41 @@
 package task.task14.task1411;
 
+import java.util.HashMap;
+
+/*
+Успеваемость студентов-2
+*/
+
 public class Solution {
+    public static HashMap<String, Double> grades = new HashMap<>();
+
+    public static void main(String[] args) {
+        addStudents();
+        System.out.println("Список студентов группы: ");
+        printStudents();
+        System.out.print("Средний балл группы: " + getAverageMark());
+    }
+
+    public static void addStudents() {
+        grades.put("Давыдов Олег", 4.3d);
+        grades.put("Шульга Николай", 4.1d);
+        grades.put("Колос Василий", 4.9d);
+        grades.put("Шевченко Тарас", 3.7d);
+        grades.put("Марчук Любослав", 3.2d);
+    }
+
+    public static void printStudents() {
+        for (String name : grades.keySet()){
+            System.out.println(name);
+        }
+    }
+
+    public static Double getAverageMark() {
+        Double sum = 0.0;
+        for (Double arg : grades.values()){
+            sum += arg;
+        }
+        return sum / grades.size();
+    }
 }
+
