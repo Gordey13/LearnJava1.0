fd9d140aaab60ab04bf6d367a9d504de4dcb75b1
Гордей <gm-liker@yandex.ru>
2022-05-29 12:02:11 +0300

    Методы класса Collections: add addAll copy ReplaceAll

--- src/main/java/task/task14/task1407/Solution.java
+++ src/main/java/task/task14/task1407/Solution.java
@@ -1,4 +1,47 @@
 package task.task14.task1407;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+Изучаем методы класса Collections, часть 1
+*/
+
 public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void copy(ArrayList<String> destination, ArrayList<String> source) {
+
+        Collections.copy(destination, source);
+
+        if(destination.size() < source.size()){
+            throw new IndexOutOfBoundsException("Размер источника превышает размер приемника")
+        }
+        for (int i = 0; i < destination.size(); i++) {
+            destination.set(i, source.get(i));
+        }
+    }
+
+    public static void addAll(ArrayList<String> list, String... strings) {
+
+        Collections.addAll(list, strings);
+
+        for(String string : strings){
+            list.add(string);
+        }
+    }
+
+    public static void replaceAll(ArrayList<String> list, String oldValue, String newValue) {
+        Collections.replaceAll(list, oldValue, newValue);
+
+        for (int i = 0; i < list.size(); i++) {
+            String string = list.get(i);
+            if(string.equals(oldValue)){
+                list.set(i, newValue);
+            }
+        }
+    }
 }
