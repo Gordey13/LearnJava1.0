8ce962b4d490f44678133e021914e44812b277a4
Гордей <gm-liker@yandex.ru>
2022-07-03 16:59:00 +0300

    Enum

--- src/main/java/task/task15/task1507/Solution.java
+++ src/main/java/task/task15/task1507/Solution.java
@@ -1,5 +1,32 @@
 package task.task15.task1507;
 
 public class Solution {
-
+    public static void main(String[] args) {
+        int temp = 36;
+        switch (temp) {
+            case 34:
+                System.out.println("Очень низкая");
+                break;
+            case 35:
+                System.out.println("Низкая");
+                break;
+            case 36:
+                System.out.println("Низкая");
+                break;
+            case 37:
+                System.out.println("Нормальная");
+                break;
+            case 38:
+                System.out.println("Высокая");
+                break;
+            case 39:
+                System.out.println("Очень высокая");
+                break;
+            case 40:
+                System.out.println("Кипяток");
+                break;
+            default:
+                System.out.println("Нужна скорая");
+        }
+    }
 }
---
+++ src/main/java/task/task15/task1508/Solution.java
@@ -0,0 +1,27 @@
+//package task.task15.task1508;
+//
+//import task.task15.task1506.Day;
+//
+//public class Solution {
+//    public static void main(String[] args) {
+//        Day day = Day.MONDAY;
+//        switch(day){
+//            case MONDAY:
+//                System.out.println("Понедельник");
+//            case TUESDAY:
+//                System.out.println("Вторник");
+//            case WEDNESDAY:
+//                System.out.println("Среда");
+//            case THURSDAY:
+//                System.out.println("Четверг");
+//            case FRIDAY:
+//                System.out.println("Пятница");
+//            case SATURDAY:
+//                System.out.println("Суббота");
+//            case SUNDAY:
+//                System.out.println("Воскресенье");
+//            default:
+//                System.out.println("Отпуск");
+//        }
+//    }
+//}
