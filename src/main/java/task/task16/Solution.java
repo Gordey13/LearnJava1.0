9674ab19a2f1aad32244c3cc77264e112f107984
Гордей <gm-liker@yandex.ru>
2022-07-09 21:31:39 +0300

    Ашибка выбросили исключенеие trows. Перехватили с помощью блока try catch

---
+++ src/main/java/task/task16/Solution.java
@@ -0,0 +1,20 @@
+package task.task16;
+
+public class Solution {
+    public static String errorMessage = "Щепка Ашибка аще чесла. Дэээ!";
+    public static void main(String[] args) {
+        try {
+            Solution.generateLuckyNumber();
+        } catch (Exception e) {
+            System.out.println(errorMessage);
+        }
+    }
+
+    static void generateLuckyNumber() throws Exception {
+        int luckyNumber = (int) (Math.random() * 100);
+        if(luckyNumber == 13) {
+            throw new Exception();
+        }
+        System.out.println("Твое счастливое число: " + luckyNumber);
+    }
+}
