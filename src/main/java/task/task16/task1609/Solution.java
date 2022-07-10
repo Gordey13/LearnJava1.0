703eada849286cf5094029aefc1ea13f2fcf64be
Гордей <gm-liker@yandex.ru>
2022-07-10 16:10:36 +0300

    try(resources) {} catch {} На try{} catch(Ex e) {} finally {}

--- src/main/java/task/task16/task1609/Solution.java
+++ src/main/java/task/task16/task1609/Solution.java
@@ -1,12 +1,25 @@
 package task.task16.task1609;
 
 /*
+Логирование стектрейса
+В методе main перехвати исключение и выведи его стектрейс.
 
+Требования:
+•	Метод main должен вызывать метод dangerousMethod.
+•	Метод dangerousMethod должен бросать Exception.
+•	Метод main должен выводить на экран стектрейс перехваченного исключения.
 */
 
 public class Solution {
-
     public static void main(String[] args) {
-        
+        try {
+            dangerousMethod();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void dangerousMethod() throws Exception {
+        throw new Exception("Mu-ha-ha!");
     }
 }
--- src/main/java/task/task16/task1610/Solution.java
+++ src/main/java/task/task16/task1610/Solution.java
@@ -1,12 +1,34 @@
 package task.task16.task1610;
 
 /*
+Возврат к истокам
+Программа считывает с консоли строку и выводит ее на экран в верхнем регистре. Но оказывается,
+она не будет работать на старых версиях Java (ниже 7й версии).
+Не меняя функциональности программы, перепиши блок try-with-resources на обычный try-catch.
+Не забудь о вызове метода close() для объектов с внешними ресурсами.
 
-*/
+Требования:
+•	Программа должна считать с консоли строку и вывести ее на экран в верхнем регистре.
+•	В программе должен использоваться блок try-catch без ресурсов.
+•	У объектов с внешними ресурсами должен быть вызван метод close() в блоке finally.
+ */
+
+import java.util.Scanner;
 
 public class Solution {
 
     public static void main(String[] args) {
-        
+        Scanner console = null;
+        try {
+            console = new Scanner(System.in);
+            String line = console.nextLine();
+            System.out.println(line.toUpperCase());
+        } catch (Exception e) {
+            System.out.println("Something went wrong : " + e);
+        } finally {
+            if(console != null) {
+                console.close();
+            }
+        }
     }
 }
